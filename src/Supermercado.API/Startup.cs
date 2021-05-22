57b424a2358c3e9688cd8a322d97ff26581c0072
adriisoliveira <adrianne.soliveira@hotmail.com>
2021-05-22 15:10:53 -0300

    Updade

--- src/Supermercado.API/Startup.cs
+++ src/Supermercado.API/Startup.cs
@@ -31,8 +31,8 @@ namespace Supermercado.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc();
             services.AddDbContext<AppDbContext>(options => {
                 options.UseInMemoryDatabase("supermercado-api-in-memory");
             });
